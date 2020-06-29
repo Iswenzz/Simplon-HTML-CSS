18f125d87f604aa5729802aeb12fdc9ef54eafe5
Iswenzz <suxlolz1528@gmail.com>
2020-06-29 12:00:19 +0200

    [E4] FIxed init script

--- courses/exo4/init.sh
+++ courses/exo4/init.sh
@@ -3,4 +3,4 @@
 cd assets
 rm -r bootstrap
 wget "https://github.com/twbs/bootstrap/archive/v4.5.0.zip" -O bootstrap.zip
-unzip bootstrap.zip && rm bootstrap.zip && mv bootstrap-4.5.0 bootstrap
+unzip bootstrap && rm bootstrap.zip && mv bootstrap-4.5.0 bootstrap
